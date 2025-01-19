4251c4b7e646a342dfdc0e4ceaa9abc52fc5ccd5
Robert King <raking45@hotmail.com>
2025-01-19 13:59:39 -0500

    Exercise 2.3 Completed

---
+++ week_2/quote.py
@@ -0,0 +1,9 @@
+""" 
+    Author: Robert King
+    Date: January 19, 2025
+    File Name: quote.py
+    Description: Uses the print function to output a quote.
+"""
+print("If you can't explain it to a six year old,")
+print("you don't understand it yourself.")
+print('- Albert Einstein')
